e20cb14f0b80da36e63caa2e6de5ee08e3c6ad8e
Debanjan Ghosh <debanjang@ymail.com>
2018-12-16 23:52:00 +0530

    Added one more test

--- app/tests/components/TodoSearch.test.jsx
+++ app/tests/components/TodoSearch.test.jsx
@@ -11,16 +11,27 @@ describe('TodoSearch', ()=>{
         expect(TodoSearch).toExist();
     });
 
-    it('should call onSearch with correct values', ()=>{
+    it('should call onSearch with correct text value from search box', ()=>{
         var spy = expect.createSpy();
         
         var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy} />);
         todoSearch.refs.searchText.value = 'Walk';
-        todoSearch.refs.showCompleted.checked = true;
+        //todoSearch.refs.showCompleted.checked = true;
         var $el = $(ReactDOM.findDOMNode(todoSearch));
 
         TestUtils.Simulate.change($el.find('input')[0]);
+        //TestUtils.Simulate.change($el.find('input')[1]);
+        expect(spy).toHaveBeenCalledWith('Walk',false);
+    });
+
+    it('should call onSearch with correct checked value from checkbox', ()=>{
+        var spy = expect.createSpy();
+        
+        var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy} />);
+        todoSearch.refs.showCompleted.checked = true;
+        var $el = $(ReactDOM.findDOMNode(todoSearch));
+
         TestUtils.Simulate.change($el.find('input')[1]);
-        expect(spy).toHaveBeenCalledWith('Walk',true);
+        expect(spy).toHaveBeenCalledWith('',true);
     });
 });
